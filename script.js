07a503697cf248968748117a46316a9a9f2d2e5c
Muhammad_Ridhwan <oneridh18@gmail.com>
2023-03-02 14:48:01 +0800

    Edit Logic Cek Jadwal

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const date = new Date();
 $(document).ready(function () {
   $('#now').click(() => {
     let hari = date.getDay();
-    let jam = date.getHours();
-    let menit = date.getMinutes();
 
-    cekSekarang(hari, jam, menit);
+    cekSekarang(hari);
   });
 
   $('#today').click(() => {
@@ -21,16 +19,18 @@ $(document).ready(function () {
 });
 
 function cekSekarang(hari) {
-  const days = ['ahad', 'senin', 'selasa', 'rabu', 'kamis', "jum'at", 'sabtu'];
-  day = days[hari];
+  // const days = ['ahad', 'senin', 'selasa', 'rabu', 'kamis', "jum'at", 'sabtu'];
+  const days = ['sabtu', 'ahad', 'senin', 'selasa', 'rabu', 'kamis', "jum'at"];
+  let day = days[hari];
 
   let harini = document.getElementsByClassName(days[hari]);
-
+  // console.log(harini);
   cekJadwal(harini, day);
 }
 
 function gettheDay(now) {
   const hari = ['ahad', 'senin', 'selasa', 'rabu', 'kamis', "jum'at", 'sabtu'];
+
   hari.forEach((element, index) => {
     // i = 0;
     let all = document.getElementsByClassName(hari[index]);
@@ -48,25 +48,30 @@ function gettheDay(now) {
 }
 
 function cekJadwal(hari, day) {
+  let j = 0;
+
   for (i = 0; i < hari.length; i++) {
     let now = new Date();
-    // now.setHours(11, 30);
+    now.setHours(16, 40);
+
+    // console.log(now, hari[i], now);
     let kolom = hari[i].getElementsByTagName('td')[1].innerHTML;
 
     let jam = kolom.slice(0, 2);
     let menit = kolom.slice(3, 5);
 
     const startTime = new Date();
-    startTime.setHours(parseInt(jam));
+    startTime.setHours(jam);
     startTime.setMinutes(parseInt(menit));
 
     const endTime = new Date();
-    endTime.setHours(kolom.slice(8, 10));
-    endTime.setMinutes(kolom.slice(11, 13));
+    endTime.setHours(kolom.slice(8, 10), kolom.slice(11, 13));
+
+    let isi = '';
+    let andau = day[0].toUpperCase() + day.substr(1);
 
     if (now >= startTime && now <= endTime) {
       // Hari dengan Capitalize
-      let andau = day[0].toUpperCase() + day.substr(1);
       let row = hari[i].getElementsByTagName('td');
 
       let jam = row[1].innerHTML;
@@ -75,15 +80,17 @@ function cekJadwal(hari, day) {
       let dosen = row[4].innerHTML;
       let ruang = row[5].innerHTML;
 
-      const info = `${jam} <br> ${matkul} (${kelas}) <br> ${dosen} <br> Ruang ${ruang}`;
-
-      Swal.fire(andau, info, 'info');
+      const info = 1;
+      isi = `${jam} <br> ${matkul} (${kelas}) <br> ${dosen} <br> Ruang ${ruang}`;
+      j = 10;
     } else {
-      let andau = day[0].toUpperCase() + day.substr(1);
+      j = j + i;
+    }
 
-      const info = `Tidak ada jadwal di jam ini`;
-
-      Swal.fire(andau, info, 'info');
+    if (j >= 10) {
+      Swal.fire(andau, isi, 'info');
+    } else {
+      Swal.fire(andau, 'Tidak ada Jadwal Sekarang', 'info');
     }
   }
 }
